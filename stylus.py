38a00088b9b9c804b811b464ca111bd542ba8f08
Shourjo Chakravarty <cvtsh248@gmail.com>
2018-06-20 18:50:14 +0800

    Improved right click detection....

--- stylus.py
+++ stylus.py
@@ -1,4 +1,5 @@
 import cv2
+from multiprocessing import Process
 import numpy as np
 import pyautogui
 pyautogui.FAILSAFE = False
@@ -19,9 +20,16 @@ x = 0
 
 y = 0
 
+clcount = 0
+
+
 while True:
 
-	mx = 0 #For tracking purposes
+	area = 0
+
+	area1 = 0
+
+	mx = 0
 
 	my = 0
 
@@ -31,22 +39,24 @@ while True:
 
 	ret, frame = cap.read()
 
-	frame = cv2.blur(frame,(10,10)) #reduce noise
+	frame = cv2.blur(frame,(10,10))
 
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	lower = np.array([100,70,30]) #Upper and lower limits
+	lower = np.array([100,70,30])
 
-	upper = np.array([179,255,255])
+	upper = np.array([140,185,185])
 
-	mask = cv2.inRange(hsv, lower, upper) #Mask
+	mask = cv2.inRange(hsv, lower, upper)
 
-	white = np.asarray(mask) #Finding white points in mask (which would be the tip of the stylus)
+	white = np.asarray(mask)
 
 	coordList = np.argwhere(white == 255)
 
+	area = len(coordList)
+
 	try:
-		for n in range(0, len(coordList)): #Find the position of the tip of the pen, average everything
+		for n in range(0, len(coordList)):
 			mxd += coordList[n][0]
 			myd += coordList[n][1]
 		mxd = mxd/len(coordList)
@@ -54,7 +64,7 @@ while True:
 	except:
 		pass
 
-	ret, frame = cap.read() #Repeating the process above to get two readings, so that we can determine the motion.
+	ret, frame = cap.read()
 
 	frame = cv2.blur(frame,(5,5))
 
@@ -62,7 +72,7 @@ while True:
 
 	lower = np.array([100,70,30])
 
-	upper = np.array([179, 255, 255])
+	upper = np.array([140, 185, 185])
 
 	mask1 = cv2.inRange(hsv1, lower, upper)
 
@@ -75,6 +85,8 @@ while True:
 	white = np.asarray(mask1)
 
 	coordList = np.argwhere(white == 255)
+
+	area1 = len(coordList)
 
 	try:
 		for n in range(0, len(coordList)):
@@ -92,33 +104,41 @@ while True:
 	movex = 0
 
 	movey = 0
-	#motion detection, in what direction
-	if mxd-mx > 3 or mxd-mx < -3:
-		if mxd-mx < 120 and mxd-mx > -120:
-			movex = (mxd-mx)*10 
-			#pyautogui.moveRel(0, (mxd-mx))
-	if myd-my > 3 or myd-my < -3:
-		if myd-my < 120 and myd-my > - 120:
-			movey = (myd - my)*10
-			#pyautogui.moveRel((myd-my),0)
+
+	clcount = 0
+	#print time.time()-start_time
+
+	if abs(area-area1) > 210:
+		if clcount < 1:
+			print "click"
+			pyautogui.click()
+		clcount += 1
+
+	if clcount < 1:
+
+		if mxd-mx > 3 or mxd-mx < -3:
+			if mxd-mx < 120 and mxd-mx > -120:
+				movex = (mxd-mx)*5
+				#pyautogui.moveRel(0, (mxd-mx))
+		if myd-my > 3 or myd-my < -3:
+			if myd-my < 120 and myd-my > - 120:
+				movey = (myd - my)*5
+				#pyautogui.moveRel((myd-my),0)
 
 	xpos, ypos = pyautogui.position()
-	pyautogui.moveTo(xpos+movey, ypos+movex) #Very rickety movement, but accurate
 
-	#print time.time()-start_time
-	#determine a click
-	if mxd-mx <= 30 and mxd-mx >= -30:
-		if movey >= 10 and movey <= 60:
-			print "click"
-			pyautogui.click()
-		if movey <= -10 and movey >= -60:
-			print "click"
-			pyautogui.click()
+	#for i in range(0,4):
 
-	print movex, movey
+	pyautogui.moveRel(movey, movex,0.025)
+
+	print movex, movey, area-area1
 
 	if cv2.waitKey(1) == 27:
 		break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
