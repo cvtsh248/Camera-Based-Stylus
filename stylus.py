4c35da1ec4c6bd167a725938d5d36e9732395735
cvtsh248 <cvtsh248@gmail.com>
2018-06-20 11:55:50 +0800

    Added Comments, neatened up

--- stylus.py
+++ stylus.py
@@ -21,7 +21,7 @@ y = 0
 
 while True:
 
-	mx = 0
+	mx = 0 #For tracking purposes
 
 	my = 0
 
@@ -31,22 +31,22 @@ while True:
 
 	ret, frame = cap.read()
 
-	frame = cv2.blur(frame,(10,10))
+	frame = cv2.blur(frame,(10,10)) #reduce noise
 
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	lower = np.array([100,70,30])
+	lower = np.array([100,70,30]) #Upper and lower limits
 
 	upper = np.array([179,255,255])
 
-	mask = cv2.inRange(hsv, lower, upper)
+	mask = cv2.inRange(hsv, lower, upper) #Mask
 
-	white = np.asarray(mask)
+	white = np.asarray(mask) #Finding white points in mask (which would be the tip of the stylus)
 
 	coordList = np.argwhere(white == 255)
 
 	try:
-		for n in range(0, len(coordList)):
+		for n in range(0, len(coordList)): #Find the position of the tip of the pen, average everything
 			mxd += coordList[n][0]
 			myd += coordList[n][1]
 		mxd = mxd/len(coordList)
@@ -54,7 +54,7 @@ while True:
 	except:
 		pass
 
-	ret, frame = cap.read()
+	ret, frame = cap.read() #Repeating the process above to get two readings, so that we can determine the motion.
 
 	frame = cv2.blur(frame,(5,5))
 
@@ -92,10 +92,10 @@ while True:
 	movex = 0
 
 	movey = 0
-
+	#motion detection, in what direction
 	if mxd-mx > 3 or mxd-mx < -3:
 		if mxd-mx < 120 and mxd-mx > -120:
-			movex = (mxd-mx)*10
+			movex = (mxd-mx)*10 
 			#pyautogui.moveRel(0, (mxd-mx))
 	if myd-my > 3 or myd-my < -3:
 		if myd-my < 120 and myd-my > - 120:
@@ -103,10 +103,10 @@ while True:
 			#pyautogui.moveRel((myd-my),0)
 
 	xpos, ypos = pyautogui.position()
-	pyautogui.moveTo(xpos+movey, ypos+movex)
+	pyautogui.moveTo(xpos+movey, ypos+movex) #Very rickety movement, but accurate
 
 	#print time.time()-start_time
-
+	#determine a click
 	if mxd-mx <= 30 and mxd-mx >= -30:
 		if movey >= 10 and movey <= 60:
 			print "click"
